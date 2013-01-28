efb9239f5bca7631b955a83b44c85d5465472f55
Jérémy RIVIERE <jeremy.riviere@etu.enseeiht.fr>
2013-01-28 22:44:07 +0000

    Clean up

---
+++ CO417-HW1/ExampleCode/Utils.hpp
@@ -0,0 +1,11 @@
+#ifndef __UTILS__HPP__
+#define __UTILS__HPP__
+
+class Utils {
+
+public:
+    enum Type {PFM, PPM};
+    convert(const char *image_in, const char *image_out, Type t);
+
+}
+#endif
